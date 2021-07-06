c0e679c8fa28c6883d44842f82b508eacf006d1c
gushengdi <gushengdi@rongcloud.cn>
2021-07-06 20:59:10 +0800

    fix: 完善界面

--- app/src/main/java/com/example/voiceroomdemo/mvp/fragment/voiceroom/musicsetting/MusicSettingFragment.kt
+++ app/src/main/java/com/example/voiceroomdemo/mvp/fragment/voiceroom/musicsetting/MusicSettingFragment.kt
@@ -6,6 +6,9 @@ package com.example.voiceroomdemo.mvp.fragment.voiceroom.musicsetting
 
 import com.example.voiceroomdemo.R
 import com.example.voiceroomdemo.mvp.fragment.BaseBottomSheetDialogFragment
+import com.example.voiceroomdemo.ui.widget.ActionSnackBar
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import kotlinx.android.synthetic.main.fragment_music_setting.*
 
 /**
  * @author gusd
@@ -14,11 +17,35 @@ import com.example.voiceroomdemo.mvp.fragment.BaseBottomSheetDialogFragment
 class MusicSettingFragment(view: IMusicSettingView) :
     BaseBottomSheetDialogFragment<MusicSettingPresenter, IMusicSettingView>(R.layout.fragment_music_setting),
     IMusicSettingView by view {
+
+    private val actionSnackBar: ActionSnackBar by lazy {
+        ActionSnackBar.make(cl_top, R.layout.layout_music_atmosphere).apply {
+            getView().setBackgroundColor(resources.getColor(R.color.transparent))
+            addCallback(object : BaseTransientBottomBar.BaseCallback<ActionSnackBar>() {
+                override fun onDismissed(transientBottomBar: ActionSnackBar?, event: Int) {
+                    super.onDismissed(transientBottomBar, event)
+                    iv_atmosphere_music.isSelected = false
+                }
+
+                override fun onShown(transientBottomBar: ActionSnackBar?) {
+                    super.onShown(transientBottomBar)
+                    iv_atmosphere_music.isSelected = true
+                }
+            })
+        }
+    }
+
     override fun initPresenter(): MusicSettingPresenter {
         return MusicSettingPresenter(this)
     }
 
     override fun initView() {
-
+        iv_atmosphere_music.setOnClickListener {
+            if (actionSnackBar.isShown) {
+                actionSnackBar.dismiss()
+            } else {
+                actionSnackBar.show()
+            }
+        }
     }
 }
