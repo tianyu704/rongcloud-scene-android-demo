a4a2779b89a3c54ffbbcb37693116811e1b850df
gushengdi <gushengdi@rongcloud.cn>
2021-07-14 15:55:34 +0800

    fix: 修改错误码

--- rtcvoiceroom/src/main/java/cn/rongcloud/voiceroom/model/VoiceRoomErrorCode.java
+++ rtcvoiceroom/src/main/java/cn/rongcloud/voiceroom/model/VoiceRoomErrorCode.java
@@ -18,62 +18,62 @@ public enum VoiceRoomErrorCode {
     /// 连接服务器失败
     RCVoiceRoomConnectTokenFailed(70001,"Init token failed"),
     /// 麦位序号不对
-    RCVoiceRoomSeatIndexOutOfRange(70000,"Seat index not correct"),
+    RCVoiceRoomSeatIndexOutOfRange(70002,"Seat index not correct"),
     /// 用户已经在麦位上
-    RCVoiceRoomUserAlreadyOnSeat(70000,"User is on seat now"),
+    RCVoiceRoomUserAlreadyOnSeat(70003,"User is on seat now"),
     /// 用户不在麦位上
-    RCVoiceRoomUserNotOnSeat(70000,"User Not on seat now"),
+    RCVoiceRoomUserNotOnSeat(70004,"User Not on seat now"),
     /// 用户跳麦的麦位和之前的一样
-    RCVoiceRoomJumpIndexEqual(70000,"Target index can't equal to current index"),
+    RCVoiceRoomJumpIndexEqual(70005,"Target index can't equal to current index"),
     /// 麦位不是空置状态
-    RCVoiceRoomSeatNotEmpty(70000,"Seat is locked or using"),
+    RCVoiceRoomSeatNotEmpty(70006,"Seat is locked or using"),
     /// 不能抱自己上麦
-    RCVoiceRoomPickSelfToSeat(70000,"User can't pick self on seat"),
+    RCVoiceRoomPickSelfToSeat(70007,"User can't pick self on seat"),
     /// 发送抱麦请求失败
-    RCVoiceRoomPickUserFailed(70000,"Pick user seat failed"),
+    RCVoiceRoomPickUserFailed(70008,"Pick user seat failed"),
     /// 不能踢自己下麦
-    RCVoiceRoomUserKickSelfFromSeat(70000,"User can't kick self"),
+    RCVoiceRoomUserKickSelfFromSeat(70009,"User can't kick self"),
     /// 加入语聊房失败
-    RCVoiceRoomJoinRoomFailed(70000,"Join ChatRoom Failed"),
+    RCVoiceRoomJoinRoomFailed(70010,"Join ChatRoom Failed"),
     /// 离开语聊房失败
-    RCVoiceRoomLeaveRoomFailed(70000,"Leave chat or rtc room failed"),
+    RCVoiceRoomLeaveRoomFailed(70011,"Leave chat or rtc room failed"),
     /// 获取房间信息失败
-    RCVoiceRoomGetRoomInfoFailed(70000,"获取房间信息失败"),
+    RCVoiceRoomGetRoomInfoFailed(70012,"获取房间信息失败"),
     /// 已经在排麦列表中了
-    RCVoiceRoomAlreadyInRequestList(70000,"User already on seat"),
+    RCVoiceRoomAlreadyInRequestList(70013,"User already on seat"),
     /// 排麦人数太多
-    RCVoiceRoomRequestListFull(70000,"Request seat list is full, the max is 20"),
+    RCVoiceRoomRequestListFull(70014,"Request seat list is full, the max is 20"),
     /// 排麦请求发送失败
-    RCVoiceRoomSendRequestSeatFailed(70000,"Send request seat failed"),
+    RCVoiceRoomSendRequestSeatFailed(70015,"Send request seat failed"),
     /// 取消排麦请求发送失败
-    RCVoiceRoomCancelRequestSeatFailed(70000,"Cancel request seat failed"),
+    RCVoiceRoomCancelRequestSeatFailed(70016,"Cancel request seat failed"),
     /// 同意排麦请求发送失败
-    RCVoiceRoomAcceptRequestSeatFailed(70000,"同意排麦请求发送失败"),
+    RCVoiceRoomAcceptRequestSeatFailed(70017,"同意排麦请求发送失败"),
     /// 拒绝排麦请求发送失败
-    RCVoiceRoomRejectRequestSeatFailed(70000,"拒绝排麦请求发送失败"),
+    RCVoiceRoomRejectRequestSeatFailed(70018,"拒绝排麦请求发送失败"),
     /// 同步麦位信息失败
-    RCVoiceRoomSyncSeatInfoFailed(70000,"sync seat info failed"),
+    RCVoiceRoomSyncSeatInfoFailed(70019,"sync seat info failed"),
     /// 同步房间信息失败
-    RCVoiceRoomSyncRoomInfoFailed(70000,"setup Room Info failed"),
+    RCVoiceRoomSyncRoomInfoFailed(70020,"setup Room Info failed"),
     /// 同步排麦相关信息
-    RCVoiceRoomSyncRequestSeatFailed(70000,"update waiting kv failed"),
+    RCVoiceRoomSyncRequestSeatFailed(70021,"update waiting kv failed"),
     /// 获取排麦请求列表失败
-    RCVoiceRoomGetRequestListFailed(70000,"Get entries failed"),
+    RCVoiceRoomGetRequestListFailed(70022,"Get entries failed"),
 
     /// 发送信息失败
-    RCVoiceRoomSendMessageFailed(70000,"Send message failed"),
+    RCVoiceRoomSendMessageFailed(70023,"Send message failed"),
     /// 请求发送失败
-    RCVoiceRoomSendInvitationSeatFailed(70000,"send invitation failed"),
+    RCVoiceRoomSendInvitationSeatFailed(70024,"send invitation failed"),
     /// 取消请求发送失败
-    RCVoiceRoomCancelInvitationFailed(70000,"cancel invitation failed"),
+    RCVoiceRoomCancelInvitationFailed(70025,"cancel invitation failed"),
     /// 同意请求发送失败
-    RCVoiceRoomAcceptInvitationFailed(70000,"accept invitation failed"),
+    RCVoiceRoomAcceptInvitationFailed(70026,"accept invitation failed"),
     /// 拒绝请求发送失败
-    RCVoiceRoomRejectInvitationFailed(70000,"reject invitation failed"),
+    RCVoiceRoomRejectInvitationFailed(70027,"reject invitation failed"),
     /// 创建语聊房失败
-    RCVoiceRoomCreateRoomFailed(70000,"Create room failed"),
+    RCVoiceRoomCreateRoomFailed(70028,"Create room failed"),
     /// 用户ID为空
-    RCVoiceRoomUserIdIsEmpty(70000,"Room or User Id is Empty");
+    RCVoiceRoomUserIdIsEmpty(70029,"Room or User Id is Empty");
 
 
     // 使用 map 提高查询效率
