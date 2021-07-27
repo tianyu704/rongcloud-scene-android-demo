aed34010653332bb394ce81252800f0ade2e94a8
gushengdi <gushengdi@rongcloud.cn>
2021-07-27 14:21:17 +0800

    feat：临时提交

--- compiler/src/main/java/cn/rongcloud/compiler/HiltInjectProcessor.kt
+++ compiler/src/main/java/cn/rongcloud/compiler/HiltInjectProcessor.kt
@@ -4,10 +4,11 @@
 
 package cn.rongcloud.compiler
 
-import com.google.auto.common.BasicAnnotationProcessor
+import cn.rongcloud.annotation.HiltBinding
 import com.google.auto.service.AutoService
-import com.google.common.collect.ImmutableList
-import javax.annotation.processing.Processor
+import javax.annotation.processing.*
+import javax.lang.model.SourceVersion
+import javax.lang.model.element.TypeElement
 import javax.tools.Diagnostic
 
 /**
@@ -15,27 +16,28 @@ import javax.tools.Diagnostic
  * @Date 2021/07/26
  */
 @AutoService(Processor::class)
-public class HiltInjectProcessor : BasicAnnotationProcessor() {
-//    override fun process(
-//        annotations: MutableSet<out TypeElement>,
-//        processingEnv: RoundEnvironment
-//    ): Boolean {
-//        return try {
-//            processImpl(annotations, processingEnv)
-//        } catch (e: Exception) {
-//            logError(e.message)
-//            true
-//        }
-//    }
-//
-//    private fun processImpl(
-//        annotations: MutableSet<out TypeElement>,
-//        processingEnv: RoundEnvironment
-//    ): Boolean {
-//        val hiltBindingElement = processingEnv.getElementsAnnotatedWith(HiltBinding::class.java)
-//        logDebug("$hiltBindingElement.size")
-//        return true
-//    }
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class HiltInjectProcessor : AbstractProcessor() {
+    override fun process(
+        annotations: MutableSet<out TypeElement>,
+        processingEnv: RoundEnvironment
+    ): Boolean {
+        return try {
+            processImpl(annotations, processingEnv)
+        } catch (e: Exception) {
+            logError(e.message)
+            true
+        }
+    }
+
+    private fun processImpl(
+        annotations: MutableSet<out TypeElement>,
+        processingEnv: RoundEnvironment
+    ): Boolean {
+        val hiltBindingElement = processingEnv.getElementsAnnotatedWith(HiltBinding::class.java)
+        logDebug("$hiltBindingElement.size")
+        return true
+    }
 
     private fun logDebug(message: String?) {
         message?.let {
@@ -53,10 +55,6 @@ public class HiltInjectProcessor : BasicAnnotationProcessor() {
                 "HiltInjectProcessor: $message"
             )
         }
-    }
-
-    override fun initSteps(): MutableIterable<ProcessingStep> {
-        return ImmutableList.of()
     }
 
 }
